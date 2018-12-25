e181d81d481f846c4dec6a15be0f5447268c4d77
phamduyuyenphuong <uyenphuong1698@gmail.com>
2018-12-25 09:24:03 +0700

    Create bai2.2.cpp

---
+++ bai2.2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+typedef struct ngaythang
+{
+	int day,month, year;
+}NgayThang;
+// chuyen so ra chuoi
+void chuoi( string st,NgayThang &x);
+int NamNhuan(int year);
+int stt(NgayThang x);
+int month[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+int main()
+{
+	ngaythang x;
+	int year;
+	string st;
+	cout <<"nhap du lieu ";
+	getline(cin,st);
+	chuoi(st,x);
+	cout<<endl;
+		NamNhuan(year); 
+	cout<<NamNhuan(x.year)<<endl;
+	cout<<stt(x);
+}
+
+void chuoi(string st,NgayThang &x) {
+	
+	 x.day = atoi(st.substr(0, 2).c_str()); 
+	 cout<<x.day;
+	x.month = atoi(st.substr(3, 2).c_str()); 
+	cout<<x.month;
+     x.year = atoi(st.substr(6, 4).c_str());
+     cout<<x.year ;
+
+}
+int NamNhuan(int year)
+{
+
+	if( (year%400==0) || (year%4==0 && year%100!=0))
+	return 1;
+	else
+	 return 0;
+
+}
+int stt(NgayThang x)
+{
+	int stt=0;
+	for(int i =0; i< x.month; i++)
+	stt+=month[i];
+	stt += x.day;
+	if(NamNhuan==0 && x.month>2) stt+=1;
+	return stt;
+
+}
+
+
