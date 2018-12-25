26788d9fada17f5e2aa68c6bd9356ccaaab269ef
phamduyuyenphuong <uyenphuong1698@gmail.com>
2018-12-25 08:52:58 +0700

    Update bai2.cpp

--- bai2.cpp
+++ bai2.cpp
@@ -1,6 +1,47 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
-struct ngaythang
+typedef struct ngaythang
 {
-	string day,month,year;
+	int day,month, year;
+}NgayThang;
+// chuyen so ra chuoi
+void chuoi(const string &st,NgayThang x);
+int NamNhuan(int year);
+int main()
+{
+	ngaythang x;
+	int year;
+	string st;
+	cout <<"nhap du lieu ";
+	getline(cin,st);
+	chuoi(st,x);
+	cout<<endl;
+	cout<<"nhap nam";
+	cin>>year;
+	NamNhuan(year); 
+	cout<<NamNhuan;
 }
+
+void chuoi(const string &st,NgayThang x) {
+	
+	 x.day = atoi(st.substr(0, 2).c_str()); 
+	 cout<<x.day;
+	x.month = atoi(st.substr(3, 2).c_str()); 
+	cout<<x.month;
+     x.year = atoi(st.substr(6, 4).c_str());
+   cout<<x.year ;
+
+}
+int NamNhuan(int year)
+{
+
+	if( (year%400==0) || (year%4==0 && year%100!=0))
+	return 1;
+	else
+	 return 0;
+
+}
+
+
+
